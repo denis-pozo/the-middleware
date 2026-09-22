250cb8e9f980956ed846c1b5020e57291f0bf43f
miguel-pardal <miguel.pardal@es.stroeer-labs.com>
2026-09-22 13:23:13 +0200

    add missing class Meta

--- weather/serializers.py
+++ weather/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
 
+from .models import WeatherSnapshot
+
 class WeatherSnapshotSerializer(serializers.ModelSerializer):
     country_code = serializers.CharField(max_length=3)
     name = serializers.CharField(max_length=100)
+
+    class Meta:
+        model = WeatherSnapshot
+        fields = ['country_code', 'name']
