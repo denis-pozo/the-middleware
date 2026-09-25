aa2b33275999f04b6ada47ed9c66c7f3baeb07fb
miguel-pardal <miguel.pardal@es.stroeer-labs.com>
2026-09-25 12:12:57 +0200

    creates more tests

--- weather/tests/clients/test_countries_client.py
+++ weather/tests/clients/test_countries_client.py
@@ -1,3 +1,8 @@
+import requests
+from weather.clients.exceptions import (
+    CountryServiceException,
+    CountryNotFoundException
+)
 from unittest.mock import Mock, patch
 
 from django.test import SimpleTestCase
@@ -10,7 +15,7 @@ class CountryClientGetByCountryNameTests(SimpleTestCase):
     def setUp(self):
         self.client = CountryClient(base_url="https://countries.example", api_key="secret", timeout=5)
 
-    @patch("weather.clients.countries_client.get")
+    @patch("requests.get")
     def test_returns_json_body_on_success(self, mock_get):
         mock_get.return_value = Mock(status_code=200, ok=True, json=lambda: {"name": "Spain"})
 
@@ -22,3 +27,35 @@ class CountryClientGetByCountryNameTests(SimpleTestCase):
             headers={"Authorization": "Bearer secret"},
             timeout=5,
         )
+
+    @patch("requests.get")
+    def test_return_country_not_found_error_on_404_response(self, mock_get):
+        mock_get.return_value = Mock(status_code=404)
+
+        with self.assertRaises(CountryNotFoundException):
+          self.client.get_by_country_name("Narnia")
+
+    @patch("requests.get")
+    def test_return_country_response_not_ok(self, mock_get):
+        mock_get.return_value = Mock(ok=False)
+
+        with self.assertRaises(CountryServiceException):
+            self.client.get_by_country_name("Spain")
+
+    @patch("requests.get")
+    def test_client_timeout(self, mock_get):
+        mock_get.side_effect = requests.exceptions.Timeout()
+
+        with self.assertRaises(CountryServiceException):
+            self.client.get_by_country_name("Spain")
+
+    @patch("requests.get")
+    def test_client_connection_error(self, mock_get):
+        mock_get.side_effect = requests.exceptions.ConnectionError()
+
+        with self.assertRaises(CountryServiceException):
+            self.client.get_by_country_name("Spain")
+    
+
+
+        
